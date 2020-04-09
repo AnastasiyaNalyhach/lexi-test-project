db44d5fa574d2c5e2399da17a6d2f8abb81db25c
Anastasiya_Nalyhach <Anastasiya.Nalyhach@parkmobile.com>
2020-04-09 16:30:36 +0300

    -create BaseTest -update TestUpToDate with data provider and negative case

--- src/test/java/TestUpToDateModel.java
+++ src/test/java/TestUpToDateModel.java
@@ -1,39 +1,63 @@
+import annotations.JSON;
 import data_managing.PojoObjectMapper;
-import data_managing.ResourceReader;
 import data_managing.models.Data;
 import data_managing.models.ThirdPartyRequest;
+import data_provider.BaseTest;
+import org.assertj.core.api.SoftAssertions;
 import org.testng.annotations.Test;
 
+import java.util.HashMap;
+import java.util.Map;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
-import static org.hamcrest.CoreMatchers.is;
-import static org.hamcrest.MatcherAssert.assertThat;
-import static org.hamcrest.Matchers.greaterThan;
 
+public class TestUpToDateModel extends BaseTest {
 
-public class TestUpToDateModel {
-    private String emailRegex = "^[A-Za-z0-9+_.-]+@[a-zA-Z0-9.-]+$";
-    private String phoneRegex = "\\d-\\d{3}-\\d{3}-\\d{4}$";
-    private String pathWithinResFolder = "json/pojo/testData.json";
+    private static final String EMAIL_DATA_INDEX = "email";
+    private static final String ID_DATA_INDEX = "id";
+    private static final String PHONE_DATA_INDEX = "phone";
 
-
-    @Test(testName = "Validate UpToDate request model")
-    public void validateUpToDateModel() {
-        String requestModelAsString = ResourceReader.getResourceAsString(pathWithinResFolder);
+    @Test(testName = "Validate UpToDate request model fields - positive", dataProvider = DATA_PROVIDER_NAME)
+    @JSON(path = "json/pojo/testDataPositive.json")
+    public void validateUpToDateModel(String testData) {
         ThirdPartyRequest thirdPartyRequest =
-                                (ThirdPartyRequest) PojoObjectMapper.map(ThirdPartyRequest.class, requestModelAsString);
+                (ThirdPartyRequest) PojoObjectMapper.map(ThirdPartyRequest.class, testData);
 
-        Data requestData = thirdPartyRequest.getData();
-        boolean isEmailValid = isStringMatches(requestData.getEmailAddress(), emailRegex);
-        boolean isPhoneValid = isStringMatches(requestData.getPhoneNumber(), phoneRegex);
+        Map<String, Boolean> validationResult = areParametersValid(thirdPartyRequest.getData());
 
-        assertThat("Check email format is valid", isEmailValid, is(true));
-        assertThat("Check userId is not zero-valued", requestData.getUnid(), greaterThan(0));
-        assertThat("Check phone number format is valid", isPhoneValid, is(true));
+        SoftAssertions.assertSoftly(softly -> {
+            softly.assertThat(validationResult.get(ID_DATA_INDEX)).as("UserId format is valid").isTrue();
+            softly.assertThat(validationResult.get(PHONE_DATA_INDEX)).as("Phone number format is valid").isTrue();
+            softly.assertThat(validationResult.get(EMAIL_DATA_INDEX)).as("Email format is valid").isTrue();
+        });
     }
 
-    private boolean isStringMatches(String source, String regEx) {
+    @Test(testName = "Validate UpToDate request model fields - negative", dataProvider = DATA_PROVIDER_NAME)
+    @JSON(path = "json/pojo/testDataNegative.json")
+    public void validateUpToDateModelNegative(String testData) {
+        ThirdPartyRequest thirdPartyRequest =
+                (ThirdPartyRequest) PojoObjectMapper.map(ThirdPartyRequest.class, testData);
+
+        Map<String, Boolean> validationResult = areParametersValid(thirdPartyRequest.getData());
+
+        SoftAssertions.assertSoftly(softly -> {
+            softly.assertThat(validationResult.get(ID_DATA_INDEX)).as("UserId format is valid").isTrue();
+            softly.assertThat(validationResult.get(PHONE_DATA_INDEX)).as("Phone number format is valid").isTrue();
+            softly.assertThat(validationResult.get(EMAIL_DATA_INDEX)).as("Email format is valid").isTrue();
+        });
+    }
+
+    private Map<String, Boolean> areParametersValid(Data requestData) {
+        Map<String, Boolean> result = new HashMap<>();
+
+        result.put(EMAIL_DATA_INDEX,isStringMatch(requestData.getEmailAddress(), EMAIL_REGEX));
+        result.put(PHONE_DATA_INDEX,isStringMatch(requestData.getPhoneNumber(), PHONE_REGEX));
+        result.put(ID_DATA_INDEX,isStringMatch(requestData.getUnid(), ID_REGEX));
+        return result;
+    }
+
+    private boolean isStringMatch(String source, String regEx) {
         Pattern pattern = Pattern.compile(regEx);
         Matcher matcher = pattern.matcher(source);
 
---
+++ src/test/java/data_provider/BaseTest.java
@@ -0,0 +1,22 @@
+package data_provider;
+
+import annotations.JSON;
+import data_managing.ResourceReader;
+import org.testng.annotations.DataProvider;
+
+import java.lang.reflect.Method;
+
+public class BaseTest {
+    protected static final String DATA_PROVIDER_NAME = "jsonDataProvider";
+    protected static final String EMAIL_REGEX = "^[A-Za-z0-9+_.-]+@[a-zA-Z0-9.-]+$";
+    protected static final String PHONE_REGEX = "^\\d-\\d{3}-\\d{3}-\\d{4}$";
+    protected static final String ID_REGEX = "\\p{XDigit}{1,32}";
+
+    @DataProvider(name = DATA_PROVIDER_NAME)
+    public Object[][] jsonProviderMethod(Method method) {
+        String path = method.getAnnotation(JSON.class).path();
+        String testData = ResourceReader.getResourceAsString(path);
+        return new Object[][]{{testData}};
+    }
+
+}
